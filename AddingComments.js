cefab36c2965dd7017a91e7d426e03c951aac800
Mohsin Raees <160493218+MohsinRaees@users.noreply.github.com>
2024-02-21 22:05:13 +0500

    Add files via upload

---
+++ AddingComments.js
@@ -0,0 +1,23 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//
+//we are adding line on addition numbers//
+console.log("-------------------------------------");
+console.log(5 + 3);
+//we are adding line on subtraction numbers//
+console.log("-------------------------------------");
+console.log(11 - 3);
+//we are adding line on multiplication numbers//
+console.log("-------------------------------------");
+console.log(2 * 4);
+//we are adding line on division numbers//
+console.log("-------------------------------------");
+console.log(16 / 2);
+console.log("-------------------------------------");
+//we are doing addition//
+console.log(1 + 7);
+//we are doing suntraction//
+console.log(13 - 5);
+//we are doing multiplication//
+console.log(2 * 4);
+//we are doing division//
+console.log(16 / 2);
